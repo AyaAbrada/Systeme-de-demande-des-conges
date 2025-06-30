033bf830a1a5b08a65306c511f2d580ae3505440
AyaAbrada <abradaaya2@gmail.com>
2025-06-30 10:16:07 +0100

    La création des component

---
+++ demandeConge/src/app/dashboard-employe/dashboard-employe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-employe',
+  imports: [],
+  templateUrl: './dashboard-employe.component.html',
+  styleUrl: './dashboard-employe.component.css'
+})
+export class DashboardEmployeComponent {
+
+}
---
+++ demandeConge/src/app/dashboard-manager/dashboard-manager.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-manager',
+  imports: [],
+  templateUrl: './dashboard-manager.component.html',
+  styleUrl: './dashboard-manager.component.css'
+})
+export class DashboardManagerComponent {
+
+}
