69991a2aac785d7142306781d2a5171ca5ee1d1b
AyaAbrada <abradaaya2@gmail.com>
2025-07-21 17:07:11 +0100

    Login -partie frontend

--- demandeConge/src/app/app.routes.ts
+++ demandeConge/src/app/app.routes.ts
@@ -3,11 +3,18 @@ import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {DemandesComponent} from './demandes/demandes.component';
 import {HistoriqueComponent} from './historique/historique.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {DashboardEmployeComponent} from './dashboard-employe/dashboard-employe.component';
+import {DashboardManagerComponent} from './dashboard-manager/dashboard-manager.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   {path: "login",component: LoginComponent},
   {path: "register",component: RegisterComponent},
   {path: "demandes",component: DemandesComponent},
-  {path: "hisrorique", component: HistoriqueComponent}
+  {path: "hisrorique", component: HistoriqueComponent},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "dashboard-employe", component: DashboardEmployeComponent},
+  {path: "dashboard-manager", component: DashboardManagerComponent},
+
 ];
--- demandeConge/src/app/login/login.component.ts
+++ demandeConge/src/app/login/login.component.ts
@@ -1,14 +1,56 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
+import {AuthServiceService} from '../services/auth-service.service';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
-  ],
+    RouterLink,
+    FormsModule,
+    CommonModule
+],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user = {
+    username : '',
+    password : '',
+    role : 'MANAGER | EMPLOYE'
+  };
+
+  message : string = "";
+
+  constructor(private authService : AuthServiceService ,
+              private router: Router) {
+  }
+
+  login(){
+    this.authService.login(this.user).subscribe({
+      next: (res : any )=>{
+        console.log('Réponse de login:' , res);
+
+        this.authService.saveToken(res.token);
+
+        const role = (res.role || '').toUpperCase().trim();
+
+        switch (role){
+          case 'MANAGER':
+            this.router.navigate(['/dashboard-manager']);
+            break;
+          case 'EMPLOYE':
+            this.router.navigate(['/employe-manager']);
+            break;
+          default:
+            this.router.navigate(['/dashboard'])
+        }
+      },
+      error: () =>{
+        this.message = 'Identifiants invalides.';
+      }
+    })
+  }
 
 }
