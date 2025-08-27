712bbcdae3a2620c79edaf7db25545766eb651f4
AyaAbrada <abradaaya2@gmail.com>
2025-08-27 14:44:09 +0100

    Les page home pour l'employé et le manager

--- demandeConge/src/app/dashboard-manager/dashboard-manager.component.ts
+++ demandeConge/src/app/dashboard-manager/dashboard-manager.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-manager',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './dashboard-manager.component.html',
   styleUrl: './dashboard-manager.component.css'
 })
 export class DashboardManagerComponent {
 
+  protected readonly document = document;
 }
